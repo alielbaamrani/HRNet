13f27c2d135abe34d9b0ad615c874d144feaa841
alielbaamrani <ali.elbaamrani21@gmail.com>
2023-09-07 11:27:10 +0200

    p14v1.0.1

--- src/components/Datepicker/Datepicker.tsx
+++ src/components/Datepicker/Datepicker.tsx
@@ -1,17 +1,26 @@
-import React from 'react'
-import type { DatePickerProps } from 'antd'
-import { DatePicker } from 'antd'
+import React from 'react';
+import type { DatePickerProps } from 'antd';
+import { DatePicker } from 'antd';
 
+/**
+ * Composant Datepicker pour la sélection de dates.
+ */
 function Datepicker() {
+  /**
+   *
+   * @param {moment.Moment | null} date - La date sélectionnée (ou null si aucune date n'est sélectionnée).
+   * @param {string} dateString - La date sous forme de chaîne de caractères (formatée selon le format spécifié).
+   */
   const onChange: DatePickerProps['onChange'] = (date, dateString) => {
-    console.log(date, dateString)
-  }
+    console.log(date, dateString);
+  };
 
   return (
     <div>
+      {/* Composant DatePicker d'Ant Design avec gestionnaire de changement défini */}
       <DatePicker onChange={onChange} />
     </div>
-  )
+  );
 }
 
-export default Datepicker
+export default Datepicker;
